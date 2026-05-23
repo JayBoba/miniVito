51a0a122842150aa77654d2226f667b23e6abf1c
JayBoba <shimattaku@gmail.com>
2026-05-23 17:17:46 +0300

    BEHOLD THE GRACE

---
+++ cmd/server/main.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/jmoiron/sqlx"
+	_ "github.com/lib/pq"
+
+	httpdelivery "mini-avito/internal/avito_service/delivery/http"
+	"mini-avito/internal/avito_service/repository"
+	"mini-avito/internal/config"
+)
+
+func main() {
+	cfg := config.New()
+
+	dsn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		cfg.DBHost, cfg.DBPort, cfg.DBUser, cfg.DBPassword, cfg.DBName)
+
+	db, err := sqlx.Connect("postgres", dsn)
+	if err != nil {
+		log.Fatalf("Failed to connect to database: %v", err)
+	}
+	defer db.Close()
+
+	repo := repository.NewPostgresRepository(db)
+	handler := httpdelivery.NewHandler(repo)
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/dbtest", handler.DBTest)
+
+	srv := &http.Server{
+		Addr:    ":8080",
+		Handler: mux,
+	}
+
+	go func() {
+		log.Println("Starting server on :8080")
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("Server error: %v", err)
+		}
+	}()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+
+	<-quit
+	log.Println("Received shutdown signal, shutting down gracefully...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatalf("Server forced to shutdown: %v", err)
+	}
+
+	log.Println("Server exited properly")
+}
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module mini-avito
 
 go 1.24.1
 
-require github.com/jmoiron/sqlx v1.4.0
+require (
+	github.com/jmoiron/sqlx v1.4.0
+	github.com/lib/pq v1.10.9
+)
